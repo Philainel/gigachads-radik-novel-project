686d9c2f1d33d7977c921b14d50692ffd07fcd0d
Philainel <toropovmat@gmail.com>
2023-12-08 14:55:42 +0500

    Fix directory on Windows

--- src/GameData/saveToFile.ts
+++ src/GameData/saveToFile.ts
@@ -2,7 +2,7 @@ import {fs, path} from "@tauri-apps/api"
 
 export default async function SaveToFile(name: string, data: string) {
 	if (!(await fs.exists("saves", {dir: fs.BaseDirectory.AppData}))) {
-		await fs.createDir("saves", {dir: fs.BaseDirectory.AppData})
+		await fs.createDir("saves", {dir: fs.BaseDirectory.AppData, recursive: true})
 	}
 	await fs.writeTextFile(await path.join("saves", `${name}.grnp`), data + '\n', {dir: fs.BaseDirectory.AppData})
 }
